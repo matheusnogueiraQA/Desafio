bd13c429f30164676870e672bff93cb3882a29df
Matheus Nogueira <matheussantiago1@hotmail.com>
2023-12-01 22:56:02 -0300

    feat: Adicionando plugin Allure

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,15 @@ module.exports = defineConfig({
     },
   },
 });
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+module.exports = defineConfig({
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+    }
+});
