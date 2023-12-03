5675aa700e7ce98d5ddb84d3f1267348a324b4f5
Matheus Nogueira <matheussantiago1@hotmail.com>
2023-12-03 19:01:01 -0300

    feat: Refatoração do código

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,28 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseURL : 'https://seubarriga.wcaquino.me/login',
+    baseURL: "https://seubarriga.wcaquino.me/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
 });
 
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 // import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            allureWriter(on, config);
-            return config;
-        }
-    }
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+  },
 });
